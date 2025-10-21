592dd3ffe3832c521dd9347246b3810786e7ea2c
SelectiveTobias <tobiassmid07@gmail.com>
2025-10-21 13:18:52 +0200

    Canged scoreboard to use a json file

--- Scoreboard.java
+++ Scoreboard.java
@@ -5,12 +5,13 @@ public class Scoreboard {
     Scanner scanner;
     File scoreboard;
     PrintWriter pw;
-    String fileNameSource = "scoreboard.txt";
+    String fileNameSource = "scoreboard.json";
 
     public void writeScore(){
+        String jsonString = String.format("{\"highscore\": %d}", newHighScore);
         try {
             pw = new PrintWriter(fileNameSource);
-            pw.println("test");
+            pw.print(jsonString);
             pw.close();
         } catch (FileNotFoundException e) {
             System.out.println("Could not open file due to");
@@ -20,16 +21,29 @@ public class Scoreboard {
 
     public void readScore(){
         try {
-            scoreboard = new File(fileNameSource);
-            scanner = new Scanner(scoreboard);
-            while (scanner.hasNextLine()) {
-                String data = scanner.nextLine();
-                System.out.println(data);
+            content = new String(Files.readAllBytes(Paths.get(fileNameSource)), 
+            standardCharsets.UTF_8);
+            key = "\"highscore\":";
+            int keyIndex = content.indexOf(key);
+            if (keyIndex == -1) {
+                System.err.println("JSON structure invalid: High score key not found.");
+                return 0;
             }
-            scanner.close();
+
+            in startIndex = keyIndex + key.length();
+            String numberString = content.substring(startIndex).trim().replace("}", "");
+
+            return Integer.parseInt(numberString);
+
         } catch (FileNotFoundException e) {
-            System.out.println("Could not open file due to");
-            System.out.println(e);
+            System.out.println("High score file not found. Starting with a score of 0.");
+            return 0; 
+        } catch (IOException e) {
+            System.err.println("Error reading high score file: " + e.getMessage());
+            return 0; 
+        } catch (NumberFormatException e) {
+            System.err.println("Error parsing high score value: " + e.getMessage());
+            return 0; 
         }
     }
 
